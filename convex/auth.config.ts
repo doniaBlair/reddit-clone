15f0271c970ea3c6b50c1da01a74431199e8bd94
Donia Blair <donia.blair@gmail.com>
2025-05-28 15:31:15 -0500

    repo init, project setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://winning-pangolin-40.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
---
+++ convex/schema.ts
@@ -0,0 +1,11 @@
+// Configure the backend
+
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    users: defineTable({
+        username: v.string(),
+        externalId: v.string()
+    }).index("byExternalId", ["externalId"]).index("byUsername", ["username"])
+});
